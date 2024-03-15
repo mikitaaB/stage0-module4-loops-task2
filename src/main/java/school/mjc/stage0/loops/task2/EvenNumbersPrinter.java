05424423d84a5099a13e33d4da498ae176f98b1d
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-15 23:57:54 +0300

    module 4. task 2. fix 2

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
-        int counter  = 1;
+        int counter = 0;
         while (counter <= printTillInclusive){
             if (counter % 2 == 0) {
                 System.out.println(counter);
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int counter = 0;
-        if (multiplyByAndToInclusive >= 0) {
+        if (multiplyByAndToInclusive > 0) {
             while (counter <= multiplyByAndToInclusive) {
                 int multiplied = counter * multiplyByAndToInclusive;
                 System.out.println(multiplied);
