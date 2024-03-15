b9e33718045e43cb69be285b5d402f95838a7946
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-16 00:32:51 +0300

    module 4. task 2. fix 6

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -3,14 +3,14 @@ package school.mjc.stage0.loops.task2;
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
         int counter = 0;
-        if (multiplyByAndToInclusive >= 0) {
+        if (multiplyByAndToInclusive > 0) {
             while (counter <= multiplyByAndToInclusive) {
                 int multiplied = counter * multiplyByAndToInclusive;
                 System.out.println(multiplied);
                 counter++;
             }
         } else {
-            while (counter > multiplyByAndToInclusive) {
+            while (counter >= multiplyByAndToInclusive) {
                 int multiplied = counter * multiplyByAndToInclusive * -1;
                 System.out.println(multiplied);
                 counter--;
